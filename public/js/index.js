65896ab0dd3d1f0d145ed61fcdc0c497597eeb8e
adityatom <nagamalliaditya3@gmail.com>
2019-11-17 17:40:53 +0530

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -18,22 +18,39 @@ console.log('Disconnected from server');
 
 // creating custom event newMessage 2
     socket.on('newMessage', function (message) {
+        
         var formattedTime = moment(message.createdAt).format('h:mm a');
-        // console.log('New Message', message);
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    jQuery('#messages').append(li);
+        var template = jQuery('#message-template').html();
+        var html = Mustache.render(template, {
+            text: message.text,
+            from: message.from,
+            createdAt: formattedTime
+        });
+        
+        jQuery('#messages').append(html);
+    //     // console.log('New Message', message);
+    // var li = jQuery('<li></li>');
+    // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    // jQuery('#messages').append(li);
     });
 
     //  new location message with anchor tag
     socket.on('newLocationMessage', function(message){
-        var li = jQuery('<li></li>');
-        var a = jQuery('<a target="_blank">My Current Location</a>');
         var formattedTime = moment(message.createdAt).format('h:mm a');
-        li.text(`${message.from} ${formattedTime}: `);
-        a.attr('href', message.url);
-        li.append(a);
-        jQuery('#messages').append(li);
+
+        var template = jQuery('#location-message-template').html();
+        var html = Mustache.render(template, {
+            from: message.from,
+            url : message.url,
+            createdAt:formattedTime
+        })
+        // var li = jQuery('<li></li>');
+        // var a = jQuery('<a target="_blank">My Current Location</a>');
+        // li.text(`${message.from} ${formattedTime}: `);
+        // a.attr('href', message.url);
+        // li.append(a);
+        // jQuery('#messages').append(li)
+        jQuery('#messages').append(html);
     });
         //4
         // socket.emit('createMessage', {
